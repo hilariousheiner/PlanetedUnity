bc2bf3c2931b81001284ee47dd12d6464934a15d
Heiner Schmidt <heiner.schmidt@gmail.com>
2026-08-21 14:05:11 +0200

    added 32 bit versions of FMix and FNV. also added comments

--- Runtime/PRNG/PRNG.cs
+++ Runtime/PRNG/PRNG.cs
@@ -5,7 +5,19 @@ using System.Text;
 namespace Planeted
 {
     public static class PRNG
-    {
+    {  
+        // MurmuHash finalizers, written by Austin Appleby:
+        // https://github.com/aappleby/smhasher/blob/master/src/MurmurHash3.cpp
+        public static uint FMix32(uint h)
+        {
+            h ^= h >> 16;
+            h *= 0x85ebca6b;
+            h ^= h >> 13;
+            h *= 0xc2b2ae35;
+            h ^= h >> 16;
+
+            return h;
+        }
         public static ulong FMix64(ulong h)
         {
             h ^= h >> 33;
@@ -16,7 +28,20 @@ namespace Planeted
 
             return h;
         }
+       
+        // FNV-1a hash functions:
+        // https://en.wikipedia.org/wiki/Fowler-Noll-Vo_hash_function
+        public static uint FNV32(string s)
+        {
+            uint h = 2166136261u; //FNV offset basis
 
+            foreach (byte b in Encoding.UTF8.GetBytes(s))
+            {
+                h ^= b;
+                h *= 16777619u; //FNV prime
+            }
+            return h;
+        }
         public static ulong FNV64(string s)
         {
             const ulong offsetBasis = 14695981039346656037UL;
@@ -32,6 +57,8 @@ namespace Planeted
             return h;
         }
 
+        // SplitMix64 by Sebastiano Vigna
+        // https://prng.di.unimi.it/splitmix64.c
         public static ulong SplitMix64(ref ulong state)
         {
             state += 0x9e3779b97f4a7c15ul;
@@ -44,6 +71,7 @@ namespace Planeted
             return z ^ (z >> 31);
         }
 
+        // Utils
         public static ulong RandomSeed()
         {
             byte[] bytes = new byte[8];
