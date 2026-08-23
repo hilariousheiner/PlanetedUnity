4d63367608d279d6adf5604bb5c95e519335e61e
Heiner Schmidt <heiner.schmidt@gmail.com>
2026-08-23 20:57:02 +0200

    added PDSL and PDSLRuntime

---
+++ Runtime/PDSL/PDSL.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+namespace Planeted
+{
+    public class PDSL
+    {
+        public static void Run(string code, PDSLRuntime runtime)
+        {
+
+        }
+    }
+}
---
+++ Runtime/PDSL/PDSLRuntime.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+namespace Planeted
+{
+    public class PDSLRuntime
+    {
+
+    }
+}
