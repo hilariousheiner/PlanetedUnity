da515388c022fee46612181a3537c67f15f90818
Heiner Schmidt <heiner.schmidt@gmail.com>
2026-08-24 21:42:32 +0200

    added TokenTypeEnum

---
+++ Runtime/PDSL/Lexer/TokenTypeEnum.cs
@@ -0,0 +1,27 @@
+namespace Planeted
+{
+    public enum TokenTypeEnum 
+    {
+        Identifier,
+
+        IntLiteral,
+        FloatLiteral,
+        BoolLiteral,
+        StringLiteral,
+        NullLiteral,
+
+        Import,
+        Return,
+
+        Equals,
+        Semicolon,
+        LParen,
+        RParen,
+        LBrack,
+        RBrack,
+        Comma,
+        Minus,
+
+        End
+    }
+}
