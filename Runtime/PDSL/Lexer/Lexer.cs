e7e54077b649dfc5821008304c2227fcf9204f05
Heiner Schmidt <heiner.schmidt@gmail.com>
2026-08-26 12:07:05 +0200

    lexer implementation

--- Runtime/PDSL/Lexer/Lexer.cs
+++ Runtime/PDSL/Lexer/Lexer.cs
@@ -18,42 +18,209 @@ namespace Planeted
 
         public Lexer(string source)
         {
-
+            this.source = source;
+            this.pos = 0;
         }
+
         public Token Next()
         {
-            return null;
+            this.readWhitespaceAndComments();
+
+            char c = this.peek();
+
+            Token result = new Token(TokenTypeEnum.End, "");
+
+            if (c == '\0')
+            {
+                return result;
+            }
+
+            if (char.IsLetter(c) || c == '_')
+            {
+                return this.readIdentifier();
+            }
+
+            if (char.IsDigit(c))
+            {
+                return this.readNumber();
+            }
+
+            if (c == '"')
+            {
+                return this.readString();
+            }
+
+            this.advance();
+
+            switch (c)
+            {
+                case '=':
+                    result.TokenType = TokenTypeEnum.Equals;
+                    result.Lexeme = "=";
+                    break;
+                case ';':
+                    result.TokenType = TokenTypeEnum.Semicolon; 
+                    result.Lexeme = ";";
+                    break;
+                case '(':
+                    result.TokenType = TokenTypeEnum.LParen; 
+                    result.Lexeme = "(";
+                    break;
+                case ')':
+                    result.TokenType = TokenTypeEnum.RParen;
+                    result.Lexeme = ")";
+                    break;
+                case '[':
+                    result.TokenType = TokenTypeEnum.LBrack;
+                    result.Lexeme = "[";
+                    break;
+                case ']':
+                    result.TokenType = TokenTypeEnum.RBrack;
+                    result.Lexeme = "]";
+                    break;
+                case ',':
+                    result.TokenType = TokenTypeEnum.Comma;
+                    result.Lexeme = ",";
+                    break;
+                case '-':
+                    result.TokenType = TokenTypeEnum.Minus;
+                    result.Lexeme = "-";
+                    break;
+                default:
+                    throw new LexerException("unexpected character: " + c, this.pos);
+            }
+            return result;
         }
 
         private void readWhitespaceAndComments()
         {
+            while (true)
+            {
+                while (char.IsWhiteSpace(this.peek()))
+                {
+                    this.advance();
+                }
 
+                if (this.peek() == '/' && this.peekNext() == '/')
+                {
+                    while (this.peek() != '\n' && this.peek() != '\0')
+                    {
+                        this.advance();
+                    }
+                    continue;
+                }
+
+                if (this.peek() == '/' && this.peekNext() == '*')
+                {
+                    this.advance(); // /
+                    this.advance(); // *
+
+                    while (true)
+                    {
+                        if (this.peek() == '\0')
+                        {
+                            throw new LexerException("Unterminated block comment.", this.pos);
+                        }
+
+                        if (this.peek() == '*' && this.peekNext() == '/')
+                        {
+                            this.advance(); // *
+                            this.advance(); // /
+                            break;
+                        }
+                        this.advance();
+                    }
+                    continue;
+                }
+                break;
+            }
         }
 
         private Token readIdentifier()
         {
-            return null;
+            string text = string.Empty;
+
+            while(char.IsLetterOrDigit(this.peek()) || this.peek() == '_')
+            {
+                text += this.advance();
+            }
+
+            if(keywordDict.ContainsKey(text))
+            {
+                return new Token(keywordDict[text], text);
+            }
+
+            return new Token(TokenTypeEnum.Identifier, text);
         }
         private Token readNumber()
         {
-            return null;
+            string text = string.Empty;
+            bool isFloat = false;
+
+            while (char.IsDigit(this.peek()) || this.peek() == '.')
+            {
+                if (this.peek() == '.')
+                {
+                    if (isFloat)
+                    {
+                        throw new LexerException("Invalid float.", this.pos);
+                    }
+                    isFloat = true;
+                }
+                text += this.advance();
+            }
+
+            if (isFloat)
+            {
+                return new Token(TokenTypeEnum.FloatLiteral, text);
+            }
+            return new Token(TokenTypeEnum.IntLiteral, text);
         }
         private Token readString()
         {
-            return null;
+            string lexeme = string.Empty;
+
+            if (this.peek() == '"')
+            {
+                this.advance(); // discard initial "
+
+                while (this.peek() != '"')
+                {
+                    if (this.peek() == '\0')
+                    {
+                        throw new LexerException("unterminated string literal.", this.pos);
+                    }
+                    lexeme += this.advance();
+                }
+                this.advance(); // discard final "
+            }
+            return new Token(TokenTypeEnum.StringLiteral, lexeme);
         }
 
         private char peek()
         {
-            return '\0';
+            if (this.pos >= this.source.Length)
+            {
+                return '\0';
+            }
+            return this.source[pos];
         }
         private char peekNext()
         {
-            return '\0';
+            int next = this.pos + 1;
+            if (next >= this.source.Length)
+            {
+                return '\0';
+            }
+            return this.source[next];
         }
         private char advance()
         {
-            return '\0';
+            if (this.pos >= this.source.Length)
+            {
+                return '\0';
+            }
+            return this.source[this.pos++];
         }
     }
 }
