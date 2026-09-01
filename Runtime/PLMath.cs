03e07f39bf50231b2153c935ea9100e1698bde65
Heiner Schmidt <heiner.schmidt@gmail.com>
2026-09-01 21:29:04 +0200

    added PLMath

---
+++ Runtime/PLMath.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Planeted
+{
+    public class PLMath
+    {
+        public static int FloorToInt(float value)
+        {
+            return (int)Math.Floor(value);
+        }
+    }
+}
