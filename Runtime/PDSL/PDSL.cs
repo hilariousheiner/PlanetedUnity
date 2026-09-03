cd02c46b7d74b98b0358b9a32622c25df27717b2
Heiner Schmidt <heiner.schmidt@gmail.com>
2026-09-03 13:33:16 +0200

    implemented PDSL.Run

--- Runtime/PDSL/PDSL.cs
+++ Runtime/PDSL/PDSL.cs
@@ -6,7 +6,10 @@ namespace Planeted
     {
         public static void Run(string code, PDSLRuntime runtime)
         {
-
+            Lexer lexer = new Lexer(code);
+            PLParser parser = new PLParser(lexer);
+            PDSLProgram program = parser.Parse();
+            program.Run(runtime);
         }
     }
 }
