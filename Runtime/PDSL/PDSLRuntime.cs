0828b47216119b397727142970d2f587ee810ccf
Heiner Schmidt <heiner.schmidt@gmail.com>
2026-09-06 22:38:02 +0200

    added environment dictionary and SetVariableValue method to runtime

--- Runtime/PDSL/PDSLRuntime.cs
+++ Runtime/PDSL/PDSLRuntime.cs
@@ -1,11 +1,27 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace Planeted
 {
     public class PDSLRuntime
     {
+        private Dictionary<string, PDSLValue> environment; 
+
         public PDSLRuntime() 
         {
+            this.environment = new Dictionary<string, PDSLValue>();
+        }
+
+        public void SetVariableValue(string name, PDSLValue value)
+        {
+            if (!this.environment.ContainsKey(name))
+            {
+                this.environment.Add(name, value);
+            }
+            else
+            {
+                this.environment[name] = value;
+            }
         }
     }
 }
